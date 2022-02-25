d32bbbf3e6ccfa26713fee560dd4f5abc81dd130
Destynee Martin <aurlie.martin@gmail.com>
2022-02-24 22:42:26 -0500

    update access key

--- main.tf
+++ main.tf
@@ -1,9 +1,12 @@
 provider "aws" {
   region = "us-east-1"
-  access_key = "AKIAR5PTR5E5OJFOWSCO"
-  secret_key = "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"
+ 
 }
 resource "aws_instance" "myfirstinstance" {
     ami = var.ami
     instance_type = var.instance_type
+
+}
+resource "aws" "name" {
+  
 }
